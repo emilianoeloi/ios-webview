25239aa4c36538cfed203b62c9408f843f8d0234
emiliano.barbosa <emiliano.barbosa@corp.globo.com>
2015-08-04 12:55:27 -0300

    Using iOS/Javascript bridge without webside

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # iOS UIWebView
 
+## Dependências
+WebViewJavascript Bridge: https://github.com/marcuswestin/WebViewJavascriptBridge
+
+AFNetworking: https://github.com/AFNetworking/AFNetworking
+
 ## Referências
 UIWebView - https://developer.apple.com/library/ios/documentation/UIKit/Reference/UIWebView_Class/
--- ioswebview/ViewController.m
+++ ioswebview/ViewController.m
@@ -7,8 +7,15 @@
 //
 
 #import "ViewController.h"
+#import "WebViewJavascriptBridge.h"
 
-@interface ViewController ()
+@interface ViewController () <UIWebViewDelegate>
+
+@property WebViewJavascriptBridge* bridge;
+@property (weak, nonatomic) IBOutlet UITextField *text;
+@property (weak, nonatomic) IBOutlet UIButton *button;
+@property (weak, nonatomic) IBOutlet UIWebView *webview;
+
 
 @end
 
@@ -16,12 +23,44 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self loadWebview];
 }
 
-- (void)didReceiveMemoryWarning {
+-(void)loadWebview{
+    self.bridge = [WebViewJavascriptBridge bridgeForWebView:_webview webViewDelegate:self handler:^(id data, WVJBResponseCallback responseCallback) {
+        NSLog(@"Received message from javascript: %@", data);
+        responseCallback(@"Right back atcha");
+    }];
+    
+    [self.bridge send:@"Well hello there"];
+    [self.bridge send:[NSDictionary dictionaryWithObject:@"Foo" forKey:@"Bar"]];
+    [self.bridge send:@"Give me a response, will you?" responseCallback:^(id responseData) {
+        NSLog(@"ObjC got its response! %@", responseData);
+    }];
+    
+    NSURL *url = [NSURL URLWithString:_text.text];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [_webview loadRequest:request];
+}
+
+-(void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark WebView Delegates
+-(BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType{
+    _text.text = request.URL.absoluteString;
+    return YES;
+}
+-(void)webViewDidFinishLoad:(UIWebView *)webView{
+    _text.text = webView.request.URL.absoluteString;
+    
+    NSString *javascript = @"document.body.style.webkitTouchCallout='none';";
+    
+        javascript = [javascript stringByAppendingString:@"function connectWebViewJavascriptBridge(e){window.WebViewJavascriptBridge?e(WebViewJavascriptBridge):document.addEventListener('WebViewJavascriptBridgeReady',function(){e(WebViewJavascriptBridge)},!1)}connectWebViewJavascriptBridge(function(e){e.init(function(e,i){alert('Received message: '+e),i&&i('Right back atcha')}),e.send('Hello from the javascript'),e.send('Please respond to this',function(e){console.log('Javascript got its response',e)})});"];
+    
+    [webView stringByEvaluatingJavaScriptFromString:javascript];
 }
 
 @end
